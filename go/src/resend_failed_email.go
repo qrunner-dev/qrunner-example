3db19cb67ce420f768ca58df54d8772f7a0e8f47
Iman Tumorang <iman@tumorang.com>
2022-07-11 22:29:42 +0700

    chore: fix example with args

--- go/src/resend_failed_email.go
+++ go/src/resend_failed_email.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/sirupsen/logrus"
 )
@@ -9,4 +10,11 @@ import (
 func main() {
 	logrus.Info("Logged Here")
 	fmt.Println("WE'RE THE CHAMPION")
+
+	args := os.Args
+	if len(args) > 1 {
+		fmt.Println("Received Arguments: Option#1", args[1:])
+		fmt.Println("Received Arguments: Option#2", os.Getenv("QRUNNER_SCRIPT_ARGS"))
+	}
+
 }
--- node/src/send-notification.js
+++ node/src/send-notification.js
@@ -1,13 +1,14 @@
 
 const chalk = require('chalk');
 
-
 function processData() {
-  process.argv.forEach((val, index) => {
-    console.log(`${index}: ${val}`)
-  })
   console.log(chalk.yellowBright('Hello world!'));
-    
+  if (process.argv.length >2) {
+    console.log("Getting your arguments:  opt#1");
+    console.log("process.argv[2]: ",process.argv[2]);
+    console.log("Getting your arguments:  opt#2");
+    console.log("process.env.QRUNNER_SCRIPT_ARGS: ",process.env.QRUNNER_SCRIPT_ARGS)
+  }
 }
 
 processData();
